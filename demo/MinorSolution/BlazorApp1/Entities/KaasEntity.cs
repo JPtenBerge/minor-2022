0893ac0615c97844f35e112fabbb47c1c059e411
JP <jp@jp.nl>
2022-12-05 14:31:51 +0100

    Blazorproject gegenereerd en basics van databinding laten zien

---
+++ demo/MinorSolution/BlazorApp1/Entities/KaasEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp1.Entities;
+
+public class KaasEntity
+{
+    public int Id { get; set; }
+
+    public string Naam { get; set; }
+
+    public string Geur { get; set; }
+
+    public string ImageUrl { get; set; }
+}
