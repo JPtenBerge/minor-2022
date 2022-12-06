1eb7b52395256c5547cf40bdc815b8b880281e15
JP <jp@jp.nl>
2022-12-06 14:09:27 +0100

    Blazor's LED en lifecycles laten zien

---
+++ demo/MinorSolution/BlazorDemo/Shared/Life.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorDemo.Shared;
+
+public partial class Life : ComponentBase, IDisposable
+{
+    private Timer? _timer;
+    
+    [Parameter] public int Getal { get; set; }
+    public Life()
+    {
+        Console.WriteLine("[life] constructor met getal: " + Getal);
+    }
+    
+    protected override void OnInitialized()
+    {
+        Console.WriteLine("[life] OnInitialized met getal: " + Getal);
+
+        _timer = new Timer(new TimerCallback(state =>
+        {
+            Console.WriteLine("Hallo vanuit timer!");
+        }), null, 1000, 1000);
+
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
+        
+        // timeouts/intervals
+        // websockets
+        // database connections
+        // - indexed database
+        // - (deprecated) web sql
+        // web workers
+        // bluetooth
+        // camera API
+        // geolocation
+    }
+}
