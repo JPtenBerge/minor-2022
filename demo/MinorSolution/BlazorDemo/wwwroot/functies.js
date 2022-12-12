6aebf4a2c4bf955a8bb9b0365fdd296b5b9650e8
JP <jp@jp.nl>
2022-12-12 16:01:02 +0100

    JS interop laten zien

---
+++ demo/MinorSolution/BlazorDemo/wwwroot/functies.js
@@ -0,0 +1,17 @@
+﻿
+function getSampleData() {
+    return 'wow!';      
+}
+
+function getPosition() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            pos => resolve({
+                latitude: pos.coords.latitude,
+                longitude: pos.coords.longitude,
+                accuracy: pos.coords.accuracy
+            }),
+            err => reject('oh nee!')
+        );
+    });
+};
---
+++ demo/MinorSolution/Demo.Shared/Entities/LocationEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.Shared.Entities;
+
+public class LocationEntity
+{
+    public decimal Latitude { get; set; }
+
+    public decimal Longitude { get; set; }
+
+    public decimal Accuracy { get; set; }
+}
